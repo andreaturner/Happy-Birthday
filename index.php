c6bb66cc140713b6dfe10ed4b94b54680c703688
Paige Israel <paigeaisrael@gmail.com>
2016-06-28 05:53:18 -0700

    Created index.php
    Created index.php,

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		  <!--[if lt IE 9]>
+		  <script src="/scripts/html5shiv.min.js"></script>
+		  <![end if]-->
+        <title>Mom is the BEST!</title>
+		  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	     <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+        <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Alegreya+Sans|Alegreya|Alegreya+SC' rel='stylesheet' type='text/css' />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="stylesheet" href="styles/style.css"/>
+    </head>
+
+    <body>
+		  <section>  <!-- [Start the header.] -->
+				<div class="header">
+					 <a href="/pages/index.php"><img src="images/tulip-header-800x199.jpg" width="800" height="199" alt="Tulip Header" /></a>
+				</div>
+				<div class="header-text-green">
+					My Mom is the BEST!!!
+				</div>
+        </section>
+         
+        <section>  <!-- [Start of the content] -->
+            <div class="header-medium-text-blue-left">
+                Why is SHE the best, you ask?
+            </div>
+            
+            <ol class="ol">
+                <li>1. She made me a ketchup bottle <img src="images/ketchup-32x75.jpg" width="32" height="75" alt="Ketchup Bottle Image" />
+                    Halloween Costume.
+                </li>
+                <li>2. She said "But I still love you" <img src="images/mom-75x52.jpg" width="75" height="52" alt="Mom Tattoo Image" />
+                    when my first boyfriend dumped me (and it made a difference).
+                </li>
+                <li>3. She moved <img src="images/house-67x75.png" width="67" height="75" alt="House Image" /> our family across town so
+                    that I could go to a better school.
+                </li>
+                <li>4. She bought me a car <img src="images/car-100x43.png" width="100" height="43" alt="Car Image" /> when I got out of the
+                    <img src="images/army-40x40.jpg" width="40" height="40" alt="US Army Seal Image" /> army.
+                </li>
+                <li>5. She loves me even though I'm - well - <img src="images/duck-44x50.jpg" width="44" height="50" alt="Duck Image" /> I'm a bit
+                    of an odd duck.
+                </li>
+            </ol>
+        </section>
+        
+        <section>
+            <div class="header-medium-text-blue-center">
+                So, Mom, for the countless things that you have done for me, Thank You.<br>
+                You're the BEST.
+            </div>
+            <div class="header-medium-text-blue-center">
+                HAPPY BIRTHDAY!  <img src="images/cake-100x98.jpg" width="100" height="98" alt="Birthday Cake Image" />
+            </div>
+        </section>
+        
+        <section>
+            <div class="header-left">
+                <img src="images/heart-75x75.jpg" width="75" height="75" alt="Heart Image" />
+            </div>
+            <div class="pic">
+                <img src="images/picture-90x90.jpg" width="90" height="90" alt="Paige Picture" />
+            </div>
+        </section>
+    </body>
+</html>
